37befbb18e2df00de398a4063854103e570981b3
ryanrearden <reardenrt@gmail.com>
2025-08-08 17:43:30 +0000

    refactored for simplicity in addr making

--- ipconverter/src/main.rs
+++ ipconverter/src/main.rs
@@ -14,35 +14,27 @@ fn main() {
 
     match Ipv4Addr::from_str(input) {
         Ok(ipv4addr) => {
-            let ipv4bit16 = bit8to16(ipv4addr);
-            makeipv6(ipv4bit16);
+            makeipv6(ipv4addr);
         }
-        Err(err) => {
-            println!("Invalid IPv4 address :(");
+        Err(error) => {
+            panic!("Invalid IPv4 address: {error:?}");
         }
     }
 }
 
-fn bit8to16(ipv4addr: Ipv4Addr) -> [u16; 2] {
 
-    let mut octets = ipv4addr.octets();
-    println!("Your address is {}", ipv4addr);
+fn makeipv6(ipv4addr: Ipv4Addr) {
 
-    let ipv4chunkvec: Vec<u16> = octets
-        .chunks_exact(2)
-        .map(|chunk| u16::from_be_bytes([chunk[0], chunk[1]]))
-        .collect(); 
-    
-    let ipv4chunkarr: [u16; 2] = ipv4chunkvec.try_into().expect("Error making u16 array from vector");
-    ipv4chunkarr
-}
+    let ipv4octs = ipv4addr.octets();
 
-fn makeipv6(ipv4chunk: [u16; 2]) {
     println!("Using 64:ff9b:: as the prefix");
+    let ipv6_prefix = Ipv6Addr::new(0x0064, 0xff9b, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000, 0x0000);
+    let mut ipv6octs = ipv6_prefix.octets();
 
-    let ipv6_addr = Ipv6Addr::new(
-        0x0064, 0xff9b, 0x0000, 0x0000, 0x0000, 0x0000, ipv4chunk[0], ipv4chunk[1]
-    );
+    let mut octaddr = [0; 16];
+    octaddr[..12].copy_from_slice(&ipv6octs[0..12]);
+    octaddr[12..].copy_from_slice(&ipv4octs[0..4]);
+    let ipv6_addr = Ipv6Addr::from(octaddr); 
 
     println!("Ipv6 address: {}", ipv6_addr);
 }
